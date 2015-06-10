d3173c551860b83d1b53d9ea409b4be83a50b86f
Jerome Bakker <jeabakker@coldtrick.com>
2015-06-10 13:56:12 +0200

    changed: header slider chooses 3 random images

--- views/default/page/elements/header_logo.php
+++ views/default/page/elements/header_logo.php
@@ -14,13 +14,15 @@ $site_url = elgg_get_site_url();
 
 <div class="theme-eersel-slider">
 	<ul class="slides">
-		<?php 
+		<?php
 	   
 			$slider_images = theme_eersel_get_slider_images();
 			if (!empty($slider_images)) {
-				foreach ($slider_images as $image_url) {
+				$slider_images_keys = array_rand($slider_images, 3);
+				
+				foreach ($slider_images_keys as $key) {
 					echo "<li>";
-					echo elgg_view("output/img", array("src" => $image_url));
+					echo elgg_view("output/img", array("src" => $slider_images[$key]));
 					echo "</li>";
 				}
 			}
