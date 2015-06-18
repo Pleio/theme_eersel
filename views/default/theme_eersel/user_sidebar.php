d3d6292397a460cd17c20e3d8fbd7eccef604c14
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-06-18 16:19:53 +0200

    added: social network links in snel naar sidebar box

--- views/default/theme_eersel/user_sidebar.php
+++ views/default/theme_eersel/user_sidebar.php
@@ -12,10 +12,10 @@ $links = json_decode($links, true);
 if ($links) {
 
 	$title = "<div class='sidebar-social'>";
-	$title .= "<i class='fa fa-facebook-square'></i>";
-	$title .= "<i class='fa fa-twitter-square'></i>";
-	$title .= "<i class='fa fa-linkedin-square'></i>";
-	$title .= "<i class='fa fa-youtube-square'></i>";
+	$title .= "<a href='http://www.facebook.com/gemeenteEersel' target='_blank'><i class='fa fa-facebook-square'></i></a>";
+	$title .= "<a href='http://www.twitter.com/gemeersel' target='_blank'><i class='fa fa-twitter-square'></i></a>";
+	$title .= "<a href='https://www.linkedin.com/company/gemeente-eersel' target='_blank'><i class='fa fa-linkedin-square'></i></a>";
+	$title .= "<a href='https://www.youtube.com/user/GemEersel' target='_blank'><i class='fa fa-youtube-square'></i></a>";
 	$title .= "</div>";
 	
 	$title .= "<h3>" . elgg_echo('theme_eersel:user_sidebar:links') . "</h3>";
